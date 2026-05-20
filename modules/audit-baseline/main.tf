91a8e7d793ba04cc9ff0921d0c5a45edf44411e3
DanHenrique <danielhenrique.al@outlook.com>
2026-05-19 23:14:48 -0300

    fix audit

--- modules/audit-baseline/main.tf
+++ modules/audit-baseline/main.tf
@@ -17,19 +17,27 @@ resource "aws_s3_bucket_policy" "trail_bucket_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "AllowCloudTrailToPutObjects"
+        Sid    = "AllowCloudTrailGetBucketAcl"
         Effect = "Allow"
         Principal = {
           Service = "cloudtrail.amazonaws.com"
         }
-        Action = [
-          "s3:GetBucketAcl",
-          "s3:PutObject"
-        ]
-        Resource = [
-          aws_s3_bucket.trail_bucket.arn,
-          "${aws_s3_bucket.trail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
-        ]
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.trail_bucket.arn
+      },
+      {
+        Sid    = "AllowCloudTrailPutObject"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.trail_bucket.arn}/AWSLogs/${data.aws_caller_identity.current.account_id}/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
       }
     ]
   })
