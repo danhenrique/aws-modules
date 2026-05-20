0eaa7045a19547b217975f86be3b9717c42386a4
DanHenrique <danielhenrique.al@outlook.com>
2026-05-19 21:59:42 -0300

    feat: add billing-baseline module

---
+++ modules/billing-baseline/main.tf
@@ -0,0 +1,18 @@
+# Configura o alarme de orçamento para monitorar gastos [cite: 103]
+resource "aws_budgets_budget" "monthly_budget" {
+  name         = "${var.product}-budget"
+  budget_type  = "COST"
+  limit_amount = var.budget_limit
+  limit_unit   = var.budget_currency
+  time_unit    = var.budget_period
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = var.target_emails
+  }
+
+  tags = merge(var.tags, local.tags)
+}
---
+++ modules/billing-baseline/variables.tf
@@ -0,0 +1,36 @@
+variable "product" {
+  description = "The name of the product or project. This will be used to name resources consistently."
+  type        = string
+}
+
+variable "budget_limit" {
+  description = "The budget limit for the product or project."
+  type        = string
+}
+
+variable "budget_currency" {
+  description = "The currency for the budget limit. Default is USD."
+  type        = string
+  default     = "USD"
+}
+
+variable "budget_period" {
+  description = "The time period for the budget. Default is MONTHLY."
+  type        = string
+  default     = "MONTHLY"
+}
+
+variable "target_emails" {
+  description = "A list of email addresses to receive budget notifications."
+  type        = list(string)
+}
+
+variable "repository" {
+  type        = string
+  description = "The github repository URL for the project. This is used for documentation and tracking purposes."
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Custom tags to apply to resources. These will be merged with default tags defined in the module."
+}
